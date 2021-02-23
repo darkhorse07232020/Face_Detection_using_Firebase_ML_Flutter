d3e733acd18609872b36b8e7ec79c2781c7edd92
Darkhorse <darkhorse0723@outlook.com>
2021-02-23 18:24:17 +0300

    First Commit for Client

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,3 @@
-
-/*
- * Created by Rishit Dagli on 2/13/20 1:25 PM
- * Copyright (c) 2020.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * You may obtain a copy of the License at-
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-
 import 'dart:io';
 import 'dart:ui' as ui;
 
@@ -25,14 +6,14 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(
-  MaterialApp(
-    title: 'Face Recognition',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: FacePage(),
-  ),
-);
+      MaterialApp(
+        title: 'Face Recognition',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FacePage(),
+      ),
+    );
 
 class FacePage extends StatefulWidget {
   @override
@@ -46,19 +27,14 @@ class _FacePageState extends State<FacePage> {
   ui.Image _image;
 
   _getImageAndDetectFaces() async {
-    final imageFile = await ImagePicker.pickImage(
-        source: ImageSource.gallery
-    );
+    final imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
       isLoading = true;
     });
     final image = FirebaseVisionImage.fromFile(imageFile);
     final faceDetector = FirebaseVision.instance.faceDetector(
-      FaceDetectorOptions(
-        mode: FaceDetectorMode.fast,
-        enableLandmarks: true
-      )
-    );
+        FaceDetectorOptions(
+            mode: FaceDetectorMode.fast, enableLandmarks: true));
     List<Face> faces = await faceDetector.processImage(image);
     if (mounted) {
       setState(() {
@@ -72,7 +48,7 @@ class _FacePageState extends State<FacePage> {
   _loadImage(File file) async {
     final data = await file.readAsBytes();
     await decodeImageFromList(data).then(
-          (value) => setState(() {
+      (value) => setState(() {
         _image = value;
         isLoading = false;
       }),
@@ -85,18 +61,18 @@ class _FacePageState extends State<FacePage> {
       body: isLoading
           ? Center(child: CircularProgressIndicator())
           : (_imageFile == null)
-          ? Center(child: Text('No image selected'))
-          : Center(
-        child: FittedBox(
-          child: SizedBox(
-            width: _image.width.toDouble(),
-            height: _image.height.toDouble(),
-            child: CustomPaint(
-              painter: FacePainter(_image, _faces),
-            ),
-          ),
-        ),
-      ),
+              ? Center(child: Text('No image selected'))
+              : Center(
+                  child: FittedBox(
+                    child: SizedBox(
+                      width: _image.width.toDouble(),
+                      height: _image.height.toDouble(),
+                      child: CustomPaint(
+                        painter: FacePainter(_image, _faces),
+                      ),
+                    ),
+                  ),
+                ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getImageAndDetectFaces,
         tooltip: 'Pick Image',
