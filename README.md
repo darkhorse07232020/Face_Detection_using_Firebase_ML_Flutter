927876a2298ef6ffb98f853f06a2e8514400d33e
Darkhorse <darkhorse0723@outlook.com>
2021-02-23 18:20:49 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Face Recognition_Flutter 
+
+## Introduction
+A face recognition app using FLutter to demonstrate the use of Firebase SDKs and edge AI with Flutter
+
+ML Kit is a mobile SDK that brings Google's machine learning expertise to Android and iOS apps in a powerful yet easy-to-use package. Whether you're new or experienced in
+machine learning, you can implement the functionality you need in just a few lines of code. There's no need to have deep knowledge of neural networks or model optimization to 
+get started. On the other hand, if you are an experienced ML developer, ML Kit provides convenient APIs that help you use your custom TensorFlow Lite models in your mobile apps.
+
+Watch this video for a better understanding-
+
+[![youtube.com/watch?v=ejrn_JHksws](http://img.youtube.com/vi/ejrn_JHksws/0.jpg)](http://www.youtube.com/watch?v=ejrn_JHksws "Introducing ML Kit")
+
+
+## Examples
+<br>
+<br>
+
+<img src="https://github.com/Rishit-dagli/Face-Recognition_Flutter/blob/master/Screenshots/example1.png" height = 400>
+<img src="https://github.com/Rishit-dagli/Face-Recognition_Flutter/blob/master/Screenshots/example2.jpeg" height = 400>
+
+## What next!
+
+You can contribute to this project, all you need to do is submit a pull request. Be sure to first read [CONTRIBUTING.md](https://github.com/Rishit-dagli/Face-Recognition_Flutter/blob/master/CONTRIBUTING.md).
---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+
+/*
+ * Created by Rishit Dagli on 2/13/20 1:25 PM
+ * Copyright (c) 2020.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at-
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+import 'dart:io';
+import 'dart:ui' as ui;
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: 'Face Recognition',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: FacePage(),
+  ),
+);
+
+class FacePage extends StatefulWidget {
+  @override
+  _FacePageState createState() => _FacePageState();
+}
+
+class _FacePageState extends State<FacePage> {
+  File _imageFile;
+  List<Face> _faces;
+  bool isLoading = false;
+  ui.Image _image;
+
+  _getImageAndDetectFaces() async {
+    final imageFile = await ImagePicker.pickImage(
+        source: ImageSource.gallery
+    );
+    setState(() {
+      isLoading = true;
+    });
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final faceDetector = FirebaseVision.instance.faceDetector(
+      FaceDetectorOptions(
+        mode: FaceDetectorMode.fast,
+        enableLandmarks: true
+      )
+    );
+    List<Face> faces = await faceDetector.processImage(image);
+    if (mounted) {
+      setState(() {
+        _imageFile = imageFile;
+        _faces = faces;
+        _loadImage(imageFile);
+      });
+    }
+  }
+
+  _loadImage(File file) async {
+    final data = await file.readAsBytes();
+    await decodeImageFromList(data).then(
+          (value) => setState(() {
+        _image = value;
+        isLoading = false;
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : (_imageFile == null)
+          ? Center(child: Text('No image selected'))
+          : Center(
+        child: FittedBox(
+          child: SizedBox(
+            width: _image.width.toDouble(),
+            height: _image.height.toDouble(),
+            child: CustomPaint(
+              painter: FacePainter(_image, _faces),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImageAndDetectFaces,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  final ui.Image image;
+  final List<Face> faces;
+  final List<Rect> rects = [];
+
+  FacePainter(this.image, this.faces) {
+    for (var i = 0; i < faces.length; i++) {
+      rects.add(faces[i].boundingBox);
+    }
+  }
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15.0
+      ..color = Colors.blue;
+
+    canvas.drawImage(image, Offset.zero, Paint());
+    for (var i = 0; i < faces.length; i++) {
+      canvas.drawRect(rects[i], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FacePainter oldDelegate) {
+    return image != oldDelegate.image || faces != oldDelegate.faces;
+  }
+}
